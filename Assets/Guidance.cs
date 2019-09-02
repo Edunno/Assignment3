5346b8daa903fbd5c1b00af02c8248ed94f78e9a
Edunno <tamericandream@hotmail.com>
2019-09-02 10:35:31 +0200

    smol

--- Assets/Guidance.cs
+++ Assets/Guidance.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Guidance : MonoBehaviour
 {
-    [SerializeField]private GameObject target;
+    [SerializeField] private GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,17 @@ public class Guidance : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target.transform);
-        transform.position = Vector3.MoveTowards(transform.position,target.transform.position,GetComponent<ChickenStats>().speed);
+        bool flag = true;
+        Collider[] cols = Physics.OverlapSphere(transform.position, 0.5f);
+        for(int i = 0; i < cols.Length; i++){
+            if(cols[i].tag == "Player"){
+                flag = false;
+            }
+        }
+        if (flag)
+        {
+            transform.LookAt(target.transform);
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, GetComponent<ChickenStats>().speed);
+        }
     }
 }
